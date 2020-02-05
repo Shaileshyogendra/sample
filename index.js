609546bdb203eb3e3d09fce5462ddfd41ebf5fc6
Shaileshyogendra <43087520+Shaileshyogendra@users.noreply.github.com>
2020-02-05 22:35:12 +0530

    added new

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+display1 = document.getElementById('display1')
+display2 = document.getElementById('display2')
+but1 = document.getElementById('but1');
+but2 = document.getElementById('but2');
+form1 = document.getElementById('form1');
+form2 = document.getElementById('form2');
 
+
+function login(){
+  display1.classList.remove('display');
+  display2.classList.add('display');
+  but2.classList.remove('color1');
+  but2.classList.add('color2');
+  but1.classList.add('color1');
+  but1.classList.remove('color2');
+  form1.classList.remove('display');
+  form2.classList.add('display');
+}
+
+function register(){
+  display1.classList.add('display');
+  display2.classList.remove('display');
+  but2.classList.add('color1');
+  but2.classList.remove('color2');
+  but1.classList.add('color2');
+  but1.classList.remove('color1');
+  form1.classList.remove('display');
+  form2.classList.add('display');
+}
+
+function registerdone(){
+  form1.classList.add('display');
+  form2.classList.remove('display');
+}
